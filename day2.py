a4964be58cad6e8acc32fade7a862a9ee054312b
Ashley Stallings <ashleystal@zillowgroup.com>
2024-12-02 12:16:27 -0600

    temp commit

--- day2.py
+++ day2.py
@@ -1,6 +1,7 @@
 import sys
 from collections import deque
 from enum import Enum
+from typing import Optional
 
 
 class Direction(Enum):
@@ -13,7 +14,6 @@ def read_reports(path: str) -> int:
         for line in f:
             report = [int(item) for item in line.split(" ")]
             try:
-                analyze_report(report)
                 if analyze_report(report):
                     result += 1
             except Exception as e:
@@ -21,40 +21,47 @@ def read_reports(path: str) -> int:
                 continue
     return result
 
-# def analyze_report(report: list[int]) -> bool:
-#     dampened = False
-#     stack = deque([report[0]])
-#     direction = 1 if report[0] - report[1] < 0 else -1
-#     for i in range(len(report)):
-#         if i == 0:
-#             continue
-#         diff = stack[len(stack)-1] - report[i]
-#         is_valid = validate(diff, direction)
-#         if is_valid:
-#             stack.append(report[i])
-#         if not is_valid and not dampened:
-#             dampened = True
-#             # keep previous, discard current
-#             if i == len(report)-1 or validate(stack[len(stack)-1]-report[i+1], direction):
-#                 continue
-#             # discard previous, validate if current fixes it
-#             else:
-#                 stack.pop()
-#                 if len(stack) != 0:
-#                     diff = stack[len(stack)-1] - report[i]
-#                     if len(stack) == 1:
-#                         direction = 1 if report[i] - report[i+1] < 0 else -1
-#                     is_valid = validate(diff, direction)
-#                 else:
-#                     is_valid = True
-#                     direction = 1 if report[i] - report[i+1] < 0 else -1
-#                 stack.append(report[i])
-#         if not is_valid and dampened:
-#             return False
-#     return True
+def analyze_report(report: list[int]) -> bool:
+    dampened = False
+    stack = deque()
+    direction = Direction.DOWN if report[0] > report[1] else Direction.UP
+    for i in range(len(report)):
+        if len(stack) == 0:
+            stack.append(report[i])
+            continue
+        # validate stack and current
+        is_valid = validate(stack[-1], report[i], direction)
+        if is_valid:
+            stack.append(report[i])
+            continue
+        if not is_valid and dampened:
+            return False
 
+        if can_skip(stack, report, i, direction):
+            dampened = True
+            if len(stack) == 1:
+                direction = Direction.DOWN if stack[-1] > report[i+1] else Direction.UP
+            continue
 
-def validate(diff: int, direction: Direction):
+        stack.pop()
+        dampened = True
+        if len(stack) == 1:
+            direction = Direction.DOWN if stack[-1] > report[i+1] else Direction.UP
+        i = i-1
+        # stack empty? fix direction
+        # validate stack and current
+        # if valid append and continue
+        # else return false
+
+    return True
+
+def can_skip(stack, report, index, direction):
+    if index == len(report) - 1 or validate(stack[-1], report[index + 1], direction):
+        return True
+    return False
+
+def validate(n1: int, n2: int, direction: Direction):
+    diff = n1 - n2
     return not (
         diff == 0 or abs(diff) > 3 or (diff > 0 and direction == Direction.UP) or (diff < 0 and direction == Direction.DOWN)
     )
--- tests_day2.py
+++ tests_day2.py
@@ -1,44 +1,59 @@
-from day2 import read_reports, validate, Direction
+from day2 import analyze_report, read_reports, validate, Direction
 
 
 def test_given_diff_greater_than_3_when_validate():
-    result = validate(4, 1)
+    result = validate(10, 4, Direction.UP)
     assert result is False
-    result = validate(-4, 1)
+    result = validate(4, 10, Direction.UP)
     assert result is False
 
 def test_given_diff_is_equal_when_validate():
-    result = validate(0, 1)
+    result = validate(1,1, Direction.UP)
     assert result is False
 
 def test_given_direction_change_when_validate():
-    result = validate(6-7, Direction.DOWN)
+    result = validate(6,7, Direction.DOWN)
     assert result is False
 
-    result = validate(7-6, Direction.UP)
+    result = validate(7,6, Direction.UP)
     assert result is False
 
 def test_given_valid_when_validate():
-    result = validate(7-6, Direction.DOWN)
+    result = validate(7,6, Direction.DOWN)
     assert result is True
-    result = validate(6-7, Direction.UP)
+    result = validate(6,7, Direction.UP)
     assert result is True
-    result = validate(7-4, Direction.DOWN)
+    result = validate(7,4, Direction.DOWN)
     assert result is True
-    result = validate(4-7, Direction.UP)
+    result = validate(4,7, Direction.UP)
     assert result is True
 
-# def test_valid():
-#     result = analyze_report([7, 6, 4, 2, 1])
-#     assert result is True
-#
-#
-#
-#
-# def test_sample():
-#     count = read_reports("./input/day2_sample.txt")
-#     assert count is 4
-#
+def test_sample():
+    count = read_reports("./input/day2_sample.txt")
+    assert count is 4
+
+def test_valid():
+    result = analyze_report([7, 6, 4, 2, 1])
+    assert result is True
+
+def test_can_skip():
+    assert analyze_report([31, 34, 33, 37, 38, 40]) is True
+
+def test_can_pop():
+    assert analyze_report([32, 35, 38, 36, 37, 40]) is True
+
+def test_can_skip_equal():
+    assert analyze_report([31, 32, 33, 33, 35, 36]) is True
+
+def test_can_fix_first_element():
+    assert analyze_report([40, 32, 31, 30, 29]) is True
+
+def test_can_fix_last_element():
+    assert analyze_report([29, 30, 31, 32, 40]) is True
+
+def test_can_change_direction():
+    assert analyze_report([29, 30, 28, 27, 26]) is True
+
 # def test_failing_test_case():
 #     assert analyze_report([32, 32, 33, 36, 38, 40]) is True
 #
@@ -48,8 +63,4 @@ def test_given_valid_when_validate():
 # def test_correctly_dampens_direction():
 #     assert analyze_report([9,8,10,11,12,13]) is True
 #
-# def test_can_dampen_current():
-#     assert analyze_report([31, 34, 33, 36, 38, 40]) is True
-#
-# def test_can_dampen_prev():
-#     assert analyze_report([31, 34, 32, 36, 38, 40]) is True
+
