b1243763c1257608c92014f8cae7ef6619888fc2
Ashley Stallings <ashleystal@zillowgroup.com>
2024-12-02 11:32:12 -0600

    implement validations

--- day2.py
+++ day2.py
@@ -1,7 +1,11 @@
 import sys
 from collections import deque
-from inspect import stack
+from enum import Enum
 
+
+class Direction(Enum):
+    UP = 1
+    DOWN = 2
 
 def read_reports(path: str) -> int:
     result = 0
@@ -17,42 +21,42 @@ def read_reports(path: str) -> int:
                 continue
     return result
 
-def analyze_report(report: list[int]) -> bool:
-    dampened = False
-    stack = deque([report[0]])
-    direction = 1 if report[0] - report[1] < 0 else -1
-    for i in range(len(report)):
-        if i == 0:
-            continue
-        diff = stack[len(stack)-1] - report[i]
-        is_valid = validate(diff, direction)
-        if is_valid:
-            stack.append(report[i])
-        if not is_valid and not dampened:
-            dampened = True
-            # keep previous, discard current
-            if i == len(report)-1 or validate(stack[len(stack)-1]-report[i+1], direction):
-                continue
-            # discard previous, validate if current fixes it
-            else:
-                stack.pop()
-                if len(stack) != 0:
-                    diff = stack[len(stack)-1] - report[i]
-                    if len(stack) == 1:
-                        direction = 1 if report[i] - report[i+1] < 0 else -1
-                    is_valid = validate(diff, direction)
-                else:
-                    is_valid = True
-                    direction = 1 if report[i] - report[i+1] < 0 else -1
-                stack.append(report[i])
-        if not is_valid and dampened:
-            return False
-    return True
+# def analyze_report(report: list[int]) -> bool:
+#     dampened = False
+#     stack = deque([report[0]])
+#     direction = 1 if report[0] - report[1] < 0 else -1
+#     for i in range(len(report)):
+#         if i == 0:
+#             continue
+#         diff = stack[len(stack)-1] - report[i]
+#         is_valid = validate(diff, direction)
+#         if is_valid:
+#             stack.append(report[i])
+#         if not is_valid and not dampened:
+#             dampened = True
+#             # keep previous, discard current
+#             if i == len(report)-1 or validate(stack[len(stack)-1]-report[i+1], direction):
+#                 continue
+#             # discard previous, validate if current fixes it
+#             else:
+#                 stack.pop()
+#                 if len(stack) != 0:
+#                     diff = stack[len(stack)-1] - report[i]
+#                     if len(stack) == 1:
+#                         direction = 1 if report[i] - report[i+1] < 0 else -1
+#                     is_valid = validate(diff, direction)
+#                 else:
+#                     is_valid = True
+#                     direction = 1 if report[i] - report[i+1] < 0 else -1
+#                 stack.append(report[i])
+#         if not is_valid and dampened:
+#             return False
+#     return True
 
 
-def validate(diff: int, direction: int):
+def validate(diff: int, direction: Direction):
     return not (
-        diff == 0 or abs(diff) > 3 or (diff > 0 and direction == 1) or (diff < 0 and direction == -1)
+        diff == 0 or abs(diff) > 3 or (diff > 0 and direction == Direction.UP) or (diff < 0 and direction == Direction.DOWN)
     )
 
 
--- tests_day2.py
+++ tests_day2.py
@@ -1,46 +1,55 @@
-from day2 import analyze_report, read_reports, validate
+from day2 import read_reports, validate, Direction
 
 
-def test_valid():
-    result = analyze_report([7, 6, 4, 2, 1])
-    assert result is True
-
-
-def test_equal():
-    result = validate(0, 1)
-    assert result is False
-
-
-def test_diff_more_than_3():
+def test_given_diff_greater_than_3_when_validate():
     result = validate(4, 1)
     assert result is False
     result = validate(-4, 1)
     assert result is False
 
-
-def test_direction_change():
-    result = validate(-1, -1)
+def test_given_diff_is_equal_when_validate():
+    result = validate(0, 1)
     assert result is False
 
-    result = validate(1, 1)
+def test_given_direction_change_when_validate():
+    result = validate(6-7, Direction.DOWN)
     assert result is False
 
+    result = validate(7-6, Direction.UP)
+    assert result is False
 
-def test_sample():
-    count = read_reports("./input/day2_sample.txt")
-    assert count is 4
+def test_given_valid_when_validate():
+    result = validate(7-6, Direction.DOWN)
+    assert result is True
+    result = validate(6-7, Direction.UP)
+    assert result is True
+    result = validate(7-4, Direction.DOWN)
+    assert result is True
+    result = validate(4-7, Direction.UP)
+    assert result is True
 
-def test_failing_test_case():
-    assert analyze_report([32, 32, 33, 36, 38, 40]) is True
-
-def test_failing_test_case_2():
-    assert analyze_report([44, 47, 48, 49, 48]) is True
-
-def test_correctly_dampens_direction():
-    assert analyze_report([9,8,10,11,12,13]) is True
-
-def test_can_dampen_current():
-    assert analyze_report([31, 34, 33, 36, 38, 40]) is True
-
-def test_can_dampen_prev():
-    assert analyze_report([31, 34, 32, 36, 38, 40]) is True
+# def test_valid():
+#     result = analyze_report([7, 6, 4, 2, 1])
+#     assert result is True
+#
+#
+#
+#
+# def test_sample():
+#     count = read_reports("./input/day2_sample.txt")
+#     assert count is 4
+#
+# def test_failing_test_case():
+#     assert analyze_report([32, 32, 33, 36, 38, 40]) is True
+#
+# def test_failing_test_case_2():
+#     assert analyze_report([44, 47, 48, 49, 48]) is True
+#
+# def test_correctly_dampens_direction():
+#     assert analyze_report([9,8,10,11,12,13]) is True
+#
+# def test_can_dampen_current():
+#     assert analyze_report([31, 34, 33, 36, 38, 40]) is True
+#
+# def test_can_dampen_prev():
+#     assert analyze_report([31, 34, 32, 36, 38, 40]) is True
