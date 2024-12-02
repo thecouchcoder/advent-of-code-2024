8f0478fddfe2814c63fd025e8621a34fdad09549
Ashley Stallings <ashleystal@zillowgroup.com>
2024-12-02 11:23:01 -0600

    temp commit

--- day2.py
+++ day2.py
@@ -1,4 +1,6 @@
 import sys
+from collections import deque
+from inspect import stack
 
 
 def read_reports(path: str) -> int:
@@ -6,24 +8,51 @@ def read_reports(path: str) -> int:
     with open(path, "r") as f:
         for line in f:
             report = [int(item) for item in line.split(" ")]
-            if analyze_report(report):
-                result += 1
+            try:
+                analyze_report(report)
+                if analyze_report(report):
+                    result += 1
+            except Exception as e:
+                print(report)
+                continue
     return result
 
-
 def analyze_report(report: list[int]) -> bool:
-    direction = -1 if report[0] - report[1] < 0 else 1  # -1 for down, 1 for up
-    for i, j in zip(report, report[1:]):
-        diff = i - j
+    dampened = False
+    stack = deque([report[0]])
+    direction = 1 if report[0] - report[1] < 0 else -1
+    for i in range(len(report)):
+        if i == 0:
+            continue
+        diff = stack[len(stack)-1] - report[i]
         is_valid = validate(diff, direction)
-        if not is_valid:
+        if is_valid:
+            stack.append(report[i])
+        if not is_valid and not dampened:
+            dampened = True
+            # keep previous, discard current
+            if i == len(report)-1 or validate(stack[len(stack)-1]-report[i+1], direction):
+                continue
+            # discard previous, validate if current fixes it
+            else:
+                stack.pop()
+                if len(stack) != 0:
+                    diff = stack[len(stack)-1] - report[i]
+                    if len(stack) == 1:
+                        direction = 1 if report[i] - report[i+1] < 0 else -1
+                    is_valid = validate(diff, direction)
+                else:
+                    is_valid = True
+                    direction = 1 if report[i] - report[i+1] < 0 else -1
+                stack.append(report[i])
+        if not is_valid and dampened:
             return False
     return True
 
 
 def validate(diff: int, direction: int):
     return not (
-        diff == 0 or abs(diff) > 3 or (diff > 0 > direction) or (diff < 0 < direction)
+        diff == 0 or abs(diff) > 3 or (diff > 0 and direction == 1) or (diff < 0 and direction == -1)
     )
 
 
--- tests_day2.py
+++ tests_day2.py
@@ -1,29 +1,46 @@
-from day2 import analyze_report, read_reports
+from day2 import analyze_report, read_reports, validate
 
 
-def test_day2_valid():
+def test_valid():
     result = analyze_report([7, 6, 4, 2, 1])
     assert result is True
 
 
-def test_day2_diff():
-    result = analyze_report([7, 7, 4, 2, 1])
+def test_equal():
+    result = validate(0, 1)
     assert result is False
 
 
-def test_day2_diff_more_than_3():
-    result = analyze_report([7, 3, 2, 1])
+def test_diff_more_than_3():
+    result = validate(4, 1)
+    assert result is False
+    result = validate(-4, 1)
     assert result is False
 
 
-def test_day2_direction_change():
-    result = analyze_report([7, 8, 4, 2, 1])
+def test_direction_change():
+    result = validate(-1, -1)
     assert result is False
 
-    result = analyze_report([1, 2, 3, 4, 3, 5])
+    result = validate(1, 1)
     assert result is False
 
 
-def test_day2_sample():
+def test_sample():
     count = read_reports("./input/day2_sample.txt")
-    assert count is 2
+    assert count is 4
+
+def test_failing_test_case():
+    assert analyze_report([32, 32, 33, 36, 38, 40]) is True
+
+def test_failing_test_case_2():
+    assert analyze_report([44, 47, 48, 49, 48]) is True
+
+def test_correctly_dampens_direction():
+    assert analyze_report([9,8,10,11,12,13]) is True
+
+def test_can_dampen_current():
+    assert analyze_report([31, 34, 33, 36, 38, 40]) is True
+
+def test_can_dampen_prev():
+    assert analyze_report([31, 34, 32, 36, 38, 40]) is True
